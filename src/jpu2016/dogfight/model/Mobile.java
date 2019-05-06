daa9d66452f15734bf5f242c6ce87bc8a4079436
Gabriel <gabriel.niculae@viacesi.fr>
2019-05-06 16:29:19 +0200

    + Ajout de la classe Mobile entière

--- src/jpu2016/dogfight/model/Mobile.java
+++ src/jpu2016/dogfight/model/Mobile.java
@@ -1,4 +1,75 @@
 package jpu2016.dogfight.model;
 
+import java.awt.*;
+import java.awt.geom.Area;
+
 public class Mobile {
+
+    private int speed;
+
+    public Mobile(Direction direction, Position position, Dimension dimension, int speed, String image){
+
+    }
+    public Direction getDirection(){
+        return getDirection();
+    }
+    public void setDirection(Direction direction){
+
+    }
+    public Point getPosition(){
+        return getPosition();
+    }
+    public Dimension getDimension(){
+        return getDimension();
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public int getWidth(){
+        return getWidth();
+    }
+    public int getHeight(){
+        return getHeight();
+    }
+    public void move(){
+
+    }
+    public void placeInArea(Area area){
+
+    }
+    public boolean isPlayer(int player){
+        return isPlayer(player);
+    }
+    private void moveUp(){
+
+    }
+    private void moveRight(){
+
+    }
+    private void moveDown(){
+
+    }
+    private void moveLeft(){
+
+    }
+    public Color getColor(){
+        return getColor();
+
+    }
+    public IDogfightModel getDogfightModel() {
+        return getDogfightModel();
+
+    }
+    public void setDogfightModel(IDogfightModel dogfightModel){
+
+    }
+    public boolean hit(){
+        return hit();
+    }
+    public boolean isWeapon(){
+        return isWeapon();
+    }
+    public Image getImage(){
+        return getImage();
+    }
 }
