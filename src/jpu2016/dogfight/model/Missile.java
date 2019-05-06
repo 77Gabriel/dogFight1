f01fe64345b98684da3871eba4f06a46234a0cf2
Gabriel <gabriel.niculae@viacesi.fr>
2019-05-06 16:57:50 +0200

    + Classe Missile

--- src/jpu2016/dogfight/model/Missile.java
+++ src/jpu2016/dogfight/model/Missile.java
@@ -1,4 +1,27 @@
 package jpu2016.dogfight.model;
 
 public class Missile {
+    private int SPEED = 4;
+    private int WIDTH = 30;
+    private int HEIGHT = 10;
+    private int MAX_DISTANCE_TRAVELED = 100;
+    private String IMAGE = "missile";
+    private int distanceTraveled = 0;
+
+    public Missile(Direction direction, Dimension dimension){
+        return;
+    }
+
+    public int getWidthWithADirection(Direction direction){
+        return getWidthWithADirection(direction);
+    }
+    public int getHeightWithADirection(Direction direction){
+        return getHeightWithADirection(direction);
+    }
+    public void move(){
+
+    }
+    public boolean isWeapon(){
+        return isWeapon();
+    }
 }
--- src/jpu2016/dogfight/model/Position.java
+++ src/jpu2016/dogfight/model/Position.java
@@ -1,4 +1,37 @@
 package jpu2016.dogfight.model;
 
 public class Position {
+    private double x;
+    private double y;
+    private double maxX;
+    private double maxY;
+
+    public Position(double x, double y, double maxX, double maxY){
+
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    protected void setMaxX(double MaxX){
+
+    }
+    protected void setMaxY(double MaxY){
+
+    }
+
+
+
 }
